65cfde3aa8881442dbfee26bdb4a13c95e78396c
Oleksii Dubchak <odubchak@softserveinc.com>
2025-06-06 10:07:39 +0200

    fixed deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/your-repo-name/' : '/',
+  //base: mode === 'production' ? '/shopping-list/' : '/',
+  base: '/shopping-list/',
   plugins: [vue()],
   server: {
     allowedHosts: ['.devenv.local.com', 'localhost'],
